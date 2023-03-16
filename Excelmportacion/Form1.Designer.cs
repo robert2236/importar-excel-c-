d6fd0fc3c058f7a31c4a59c6472299be2abae1c5
robertjesus2236 <rcuberos@fibra360.net>
2023-03-16 10:20:31 -0400

    Agregar archivos de proyecto.

---
+++ Excelmportacion/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Excelmportacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataDetalles = new System.Windows.Forms.DataGridView();
+            this.btnImportarDatos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataDetalles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataDetalles
+            // 
+            this.DataDetalles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataDetalles.Location = new System.Drawing.Point(26, 30);
+            this.DataDetalles.Name = "DataDetalles";
+            this.DataDetalles.Size = new System.Drawing.Size(740, 375);
+            this.DataDetalles.TabIndex = 1;
+            // 
+            // btnImportarDatos
+            // 
+            this.btnImportarDatos.Location = new System.Drawing.Point(842, 59);
+            this.btnImportarDatos.Name = "btnImportarDatos";
+            this.btnImportarDatos.Size = new System.Drawing.Size(97, 48);
+            this.btnImportarDatos.TabIndex = 2;
+            this.btnImportarDatos.Text = "IMPORTAR";
+            this.btnImportarDatos.UseVisualStyleBackColor = true;
+            this.btnImportarDatos.Click += new System.EventHandler(this.btnImportarDatos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 450);
+            this.Controls.Add(this.btnImportarDatos);
+            this.Controls.Add(this.DataDetalles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataDetalles)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView DataDetalles;
+        private System.Windows.Forms.Button btnImportarDatos;
+    }
+}
+
---
+++ Excelmportacion/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excelmportacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataView ImportarDatos(string nombrearchivo)
+        {
+            string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", nombrearchivo);
+
+            OleDbConnection conector = new OleDbConnection(conexion);
+
+            conector.Open();
+
+            OleDbCommand consulta = new OleDbCommand("select * from [Factura$]", conector);
+
+            OleDbDataAdapter adaptador = new OleDbDataAdapter
+            {
+                SelectCommand = consulta
+            };
+
+            DataSet ds = new DataSet();
+
+            adaptador.Fill(ds);
+
+            adaptador.Fill(ds);
+
+            conector.Close();
+
+            return ds.Tables[0].DefaultView;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImportarDatos_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Excel | *xls;*.xlsx",
+                Title = "Seleccionar Archivo"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DataDetalles.DataSource = ImportarDatos(openFileDialog.FileName);
+            }
+
+        }
+    }
+}
